e80333795e83af5cffef40a794bb1898837395f1
miloazul1057 <131510703+miloazul1057@users.noreply.github.com>
2023-04-22 00:25:06 -0600

    Add files via upload

---
+++ Ej3/src/ej3/Ej3.java
@@ -0,0 +1,21 @@
+package ej3;
+
+public class Ej3 {
+
+    public static void main(String[] args) {
+        
+        /*Determine el valor de Y= X*C-2 donde C es una constante con valor C=2.5.
+        
+        a) Sabiendo que X=2 */
+     
+        double x, c, y;
+        x = 2;
+        c = 2.5;
+
+        y = x * c - 2;
+
+        System.out.println("El valor de Y es: " + y);
+        
+    }
+    
+}
